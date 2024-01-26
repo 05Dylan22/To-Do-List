a47e6c8ff3bbfddbc10cb88c37a78854918bb4f2
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-25 23:23:59 -0500

    To-Do List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [todoItems, setTodoItems] = useState([])
+
+  function addTodoItem (e) {
+    e.preventDefault()
+    setTodoItems(prev => [...prev, inputValue])
+    setInputValue("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="add-item">Add New Item</label>
+        <input autoFocus value={inputValue} onInput={e => setInputValue(e.target.value)} id='add-item' type="text" placeholder='ex: Make Dinner'/>
+        <button onClick={addTodoItem}> Add Item</button>
+      </form>
+      <h1>TO DO:</h1>
+        <ul>
+          {todoItems.map((item, index) => (
+              <li key={index}>{item}
+                <button onClick={(e) => {
+                  setTodoItems(curr => curr.filter((el) => el !== item))
+                  }}>X</button>
+              </li>
+          ))}
+      </ul>
     </div>
   );
 }
